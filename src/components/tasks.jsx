6af5185a6a6d2856dc9fd6c7970d89115ea4d4e7
tjzetty <tjzetty@mtu.edu>
2023-06-15 11:24:48 -0400

    Delete Task and Edit Task Name Buttons

--- src/components/tasks.jsx
+++ src/components/tasks.jsx
@@ -3,25 +3,71 @@ import TaskModal from './task_modal';
 
 const Tasks = ({ tasks, setTasks }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
 
   const handleTaskCreate = (task) => {
     setTasks((prevTasks) => [...prevTasks, task]);
     setIsModalOpen(false);
   };
 
+  const handleDeleteTask = (index) => {
+    setTasks((prevTasks) => {
+      const updatedTasks = [...prevTasks];
+      updatedTasks.splice(index, 1);
+      return updatedTasks;
+    });
+  };
+
+  const handleRenameTask = (index) => {
+    const newName = prompt('Enter a new name for the task:');
+    if (newName) {
+      setTasks((prevTasks) => {
+        const updatedTasks = [...prevTasks];
+        updatedTasks[index].name = newName;
+        return updatedTasks;
+      });
+    }
+  };
+
   const tableStyle = {
     borderCollapse: 'collapse',
-    borderRadius: '8px', // Add border radius to the table
+    borderRadius: '8px',
   };
 
   const cellStyle = {
+    position: 'relative',
     height: '3em',
     minWidth: '3em',
-    border: '1px solid white', // Add a border to each cell
+    border: '1px solid white',
+    cursor: 'default',
   };
 
   const headerCellStyle = {
     fontWeight: 'bold',
+  };
+
+  const buttonStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    zIndex: '1',
+    display: 'block', // Updated to 'block' instead of 'none'
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: '50%',
+    width: '24px',
+    height: '24px',
+    textAlign: 'center',
+    lineHeight: '24px',
+    cursor: 'pointer',
+  };
+
+  const handleCellHover = (index) => {
+    setHoveredIndex(index);
+  };
+
+  const handleCellLeave = () => {
+    setHoveredIndex(null);
   };
 
   return (
@@ -40,7 +86,29 @@ const Tasks = ({ tasks, setTasks }) => {
           ) : (
             tasks.map((task, index) => (
               <tr key={index}>
-                <td style={cellStyle}>{task.name}</td>
+                <td
+                  style={cellStyle}
+                  onMouseEnter={() => handleCellHover(index)}
+                  onMouseLeave={handleCellLeave}
+                >
+                  {task.name}
+                  {hoveredIndex === index && (
+                    <div>
+                      <button
+                        style={{ ...buttonStyle, top: '35%', backgroundColor: 'red' }}
+                        onClick={() => handleDeleteTask(index)}
+                      >
+                        X
+                      </button>
+                      <button
+                        style={{ ...buttonStyle, top: '65%', backgroundColor: 'orange' }}
+                        onClick={() => handleRenameTask(index)}
+                      >
+                        ~
+                      </button>
+                    </div>
+                  )}
+                </td>
               </tr>
             ))
           )}
